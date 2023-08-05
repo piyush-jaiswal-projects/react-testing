a193252a22014b4a700ce8b06b2342584f447809
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-08-05 10:58:52 +0530

    add link test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,14 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId('learn-link') 
   expect(linkElement).toBeInTheDocument();
 });
+
+//using test to identify and test element is not a good strategy because text might change
+
+test('URL is correct', () => {
+  render(<App />)
+  const linkElement = screen.getByTestId('learn-link')
+  expect(linkElement.href).toContain('https://www.portfolio1610.vercel.app')
+})
