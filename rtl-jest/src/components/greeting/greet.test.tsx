5afa1e14962bbc7d19f032bf4d0c8c4b3606b32b
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-08-05 17:07:08 +0530

    Unit test and TDD

---
+++ rtl-jest/src/components/greeting/greet.test.tsx
@@ -0,0 +1,16 @@
+// Greet should render text hello and if name is passed into the comp it should render Hello followed by name
+
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./greet";
+
+test('Greet renders correctly', () => {
+    render(<Greet />)
+    const textEl = screen.getByText("Hello")
+    expect(textEl).toBeInTheDocument()
+})
+
+test('Greet render hello with a name', () => {
+    render(<Greet name="Piyush" />)
+    const textEl = screen.getByText("Hello Piyush")
+    expect(textEl).toBeInTheDocument()
+})
---
+++ rtl-jest/src/components/greeting/greet.tsx
@@ -0,0 +1,11 @@
+type GreetProps = {
+    name?: string
+}
+
+export const Greet = (props: GreetProps) => {
+    return (
+        <div>
+            Hello {props.name}
+        </div>
+    )
+}
